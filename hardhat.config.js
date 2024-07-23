2d71732ac19ec70bd09280eed4278beb0d82c5b3
scooby-kz <scoobybest5@gmail.com>
2024-07-23 12:39:55 +0500

    Second commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["0x1e93611f6596e6373509c19748a717b856718e5b3245f06b6032ea5fb140b1ac"], //Your private key starting with "0x" 
+      accounts: ["imitted"], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
